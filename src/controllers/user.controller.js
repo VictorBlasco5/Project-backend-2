ad80559f074bb72001b52b10b270d0f20eba603e
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-03-08 21:34:24 +0100

    feat: updateProfile created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ export const getUsers = async (req, res) => {
     try {
 
         const users = await User.find().select("-password")
-            
+
         res.status(200).json({
             success: true,
             message: "users retrieved successfully",
@@ -32,29 +32,60 @@ export const getUserProfile = async (req, res) => {
             {
                 _id: userId
             }
-        ).select("-password")
-
-       if(!userProfile) {
-        return res.status(404).json(
-            {
-                success: false,
-                message: "User not found"
-            }
-        )
-       }
+        ).select("-password") // oculto la contraseña
+        if (!userProfile) {
+            return res.status(404).json(
+                {
+                    success: false,
+                    message: "User not found"
+                }
+            )
+        }
 
         res.status(200).json({
             success: true,
-            message: "users retrieved successfully",
+            message: "User retrieved successfully",
             data: userProfile
-          
-            
         })
 
     } catch (error) {
         res.status(500).json({
             success: false,
-            message: "It is not possible to recover the users",
+            message: "It is not possible to recover the user",
+            error: error
+        })
+    }
+}
+
+//Actualizar datos
+export const updateProfile = async (req, res) => {
+    try {
+
+        const userId = req.tokenData.userId;
+        const email = req.body.email
+
+        const userProfile = await User.findOneAndUpdate(
+            {
+                _id: userId
+            },
+            {
+                email: email
+            },
+            {
+                new: true
+            }
+        )
+        
+        res.status(200).json({
+            success: true,
+            message: "User update successfully",
+            data: userProfile
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "It is not possible to update the user",
             error: error
         })
     }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getUserProfile, getUsers } from "../controllers/user.controller.js";
+import { getUserProfile, getUsers, updateProfile } from "../controllers/user.controller.js";
 import {auth} from "../middlewares/auth.js"
 
 const router = Router();
 
 router.get('/', getUsers)
 router.get('/profile',auth, getUserProfile)
+router.put('/profile',auth, updateProfile)
 
 export default router;
