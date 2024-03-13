29e17fb7eb38964e268e6ce502b6d86347f99ff8
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-03-13 18:39:55 +0100

    update: refactor errors in register with handleError

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,15 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken"
+import { handleError } from "../utils/handleError.js";
 
 export const register = async (req, res) => {
-    try {
+	try {
 		const name = req.body.name
-        const email = req.body.email
-        const password = req.body.password
+		const email = req.body.email
+		const password = req.body.password
 
-		if(!name) {
+		if (!name) {
 			return res.status(400).json(
 				{
 					success: false,
@@ -17,44 +18,54 @@ export const register = async (req, res) => {
 			)
 		}
 
-        if (password.length < 6 || password.length > 10) {
-            return res.status(400).json({
-                success: false,
-                message: "Password must contain between 6 and 10 characters"
-            })
-        }
+		if (password.length < 6 || password.length > 10) {
+			// return res.status(400).json({
+			//     success: false,
+			//     message: "Password must contain between 6 and 10 characters"
+			// })
+			throw new Error('Password must contain between 6 and 10 characters')
+		}
 
-        const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-        if (!validEmail.test(email)) {
-            return res.status(400).json(
-                {
-                    success: false,
-                    message: "format email invalid"
-                }
-            )
-        }
-        const passwordEncrypted = bcrypt.hashSync(password, 5)
+		const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+		if (!validEmail.test(email)) {
+			// return res.status(400).json(
+			//     {
+			//         success: false,
+			//         message: "format email invalid"
+			//     }
+			// )
+			throw new Error('Format email invalid')
+		}
+		const passwordEncrypted = bcrypt.hashSync(password, 5)
 
-        const newUser = await User.create(
-            {
+		const newUser = await User.create(
+			{
 				name: name,
-                email: email,
-                password: passwordEncrypted
-            }
-        )
+				email: email,
+				password: passwordEncrypted
+			}
+		)
 
-        res.status(201).json({
-            success: true,
-            message: "User registered succesfully",
-            data: newUser
-        })
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: "User cant be registered",
-            error: error
-        })
-    }
+		res.status(201).json({
+			success: true,
+			message: "User registered succesfully",
+			data: newUser
+		})
+	} catch (error) {
+		// res.status(500).json({
+		//     success: false,
+		//     message: "User cant be registered",
+		//     error: error
+		// })
+		if (error.message === 'Password must contain between 6 and 10 characters') {
+			handleError(res, error.message, 404)
+		}
+		if (error.message === 'Format email invalid') {
+			handleError(res, error.message, 404)
+		}
+
+		handleError(res, "User cant be registered", 500)
+	}
 }
 
 export const login = async (req, res) => {
@@ -113,7 +124,7 @@ export const login = async (req, res) => {
 		res.status(200).json({
 			success: true,
 			message: "User logged succesfully",
-			token: token, //MOSTRAMOS EL TOKEN DE MANERA TEMPORAL PARA PODER PROBAR CON ÉL OTRA FUNCIONALIDADES
+			token: token
 		});
 	} catch (error) {
 		res.status(500).json({
